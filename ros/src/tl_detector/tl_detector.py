b02365241a2b8ab419f29930add88ce9741fb6b5
JC <leejchris21@gmail.com>
2017-12-15 22:55:29 -0700

    add training folder housekeeping

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -10,9 +10,12 @@ import tf
 import cv2
 import yaml
 import math
-from time import sleep
 from operator import itemgetter
+# record training images
+from time import sleep
 import uuid
+import os
+import shutil
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -65,6 +68,14 @@ class TLDetector(object):
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
         # enable/disable image recording for the use of training data
         if self.config['data_record_flag']:
+            # folder housekeeping
+            base_dir = 'training'
+            if os.path.exists(os.path.abspath(base_dir)):
+                shutil.rmtree(os.path.abspath((base_dir))
+            for _, light_state in self.state_txt.items():
+                light_dir = os.path.join(base_dir, light_state)
+                os.mkdir(light_dir)
+            # subscribe to image topic
             sub7 = rospy.Subscriber('/image_color', Image, self.record_training_data_callback, queue_size=1)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', TrafficLightWaypoint, queue_size=1)
@@ -72,6 +83,7 @@ class TLDetector(object):
         rospy.spin()
 
     def record_training_data_callback(self, msg):
+        sec_to_sleep = 0.1
         light_wp, line_wp, state = self.process_traffic_lights()
         print('[record_training_data_callback] state: ', state)
         try:
@@ -86,20 +98,22 @@ class TLDetector(object):
                 # cv2.imwrite('training/red/red_image_raw%s.jpeg' % (str(uuid.uuid4())), cv2_img)
                 # cv2.imwrite('training/red/red_image_resize%s.jpeg' % (str(uuid.uuid4())), resize_image)
                 cv2.imwrite('training/red/red_image_crop_%s.jpeg' % (str(uuid.uuid4())), cropped_image)
-                sleep(1)
+                sleep(sec_to_sleep)
             if state == 1:
                 # cv2.imwrite('training/yellow/yellow_image_raw%s.jpeg' % (str(uuid.uuid4())), cv2_img)
                 # cv2.imwrite('training/yellow/yellow_image_resize%s.jpeg' % (str(uuid.uuid4())), resize_image)
                 cv2.imwrite('training/yellow/yellow_image_crop_%s.jpeg' % (str(uuid.uuid4())), cropped_image)
+                sleep(sec_to_sleep)
             if state == 2:
                 # cv2.imwrite('training/green/green_image_raw%s.jpeg' % (str(uuid.uuid4())), cv2_img)
                 # cv2.imwrite('training/green/green_image_resize%s.jpeg' % (str(uuid.uuid4())), resize_image)
                 cv2.imwrite('training/green/green_image_crop_%s.jpeg' % (str(uuid.uuid4())), cropped_image)
+                sleep(sec_to_sleep)
             if state == 4:
                 # cv2.imwrite('training/unknown/unknown_image_raw%s.jpeg' % (str(uuid.uuid4())), cv2_img)
                 # cv2.imwrite('training/unknown/unknown_image_resize%s.jpeg' % (str(uuid.uuid4())), resize_image)
                 cv2.imwrite('training/unknown/unknown_image_crop_%s.jpeg' % (str(uuid.uuid4())), cropped_image)
-                sleep(1)
+                sleep(sec_to_sleep)
 
     def pose_cb(self, msg):
         self.pose = msg
